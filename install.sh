8bfe1a9d8d5bcc5ac9c564b37894c339549420c4
Hans Klunder <hans.klunder@xs4all.nl>
2015-02-05 16:39:18 +0000

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd ~
+wget http://nodejs.org/dist/v0.10.28/node-v0.10.28-linux-arm-pi.tar.gz
+tar -xzf node-v0.10.28.tar.gz
+sudo mv node-v0.10.28-linux-arm-pi/* /usr/local
+git clone git://github.com/quick2wire/quick2wire-gpio-admin.git
+cd quick2wire-gpio-admin
+make
+sudo make install
+sudo adduser $USER gpio
+cd ~/gpio-rest
+npm install express
+npm install pi-gpio
+
+# to run: node server.js
