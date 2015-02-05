9f30ae9944b086e09e2623a06eb6942f1c494e01
Hans Klunder <hans.klunder@xs4all.nl>
2015-02-05 14:20:44 +0000

    First version with I/O activated

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 var host = process.env.IP || 'localhost';
 var port = process.env.PORT || 8080;
+var toggleTime = 500 //millisecs
 
 
 var staticSite = __dirname + '/public';
 
 // start the serial reader in the same process
-//require('./readSerial');
 
 
+var gpio = require("pi-gpio");
+var hwPins=[7,11,12,13,15,16,18,22]; // Physical pins as listed at: https://www.npmjs.com/package/pi-gpio
+var gpios={};
+var gpioList=[];
 
-
-//var gpio = require("pi-gpio");
-var hwPins=[7,11,12,13,15,16,18,22] // Physical pins as listed at: https://www.npmjs.com/package/pi-gpio
-var gpios={}
-var gpioList=[]
-
-var maxGpio=0 //relays are numbered from 1 to 8
+var maxGpio=0; //relays are numbered from 1 to 8
 for (var pin in hwPins) {
-    maxGpio++
-    gpios[maxGpio]={ 'id':maxGpio,'gpio_pin':hwPins[pin] }
-    gpioList.push(maxGpio)
+    maxGpio++;
+    gpios[maxGpio]={ 'id':maxGpio,'gpio_pin':hwPins[pin] };
+    gpioList.push(maxGpio);
 }
 
 // open gpio's as input for starters
@@ -30,11 +28,27 @@ for (var pin in hwPins) {
 // setDirection
 // 
 
-//gpio.open(16, "output", function(err) {     // Open pin 16 for output 
-//    gpio.write(16, 1, function() {          // Set pin 16 high (1) 
-//        gpio.close(16);                     // Close pin 16 
-//    });
-//});
+function togglePin(hwPin){
+  gpio.open(hwPin, "output", function(err) {    // Open pin for output 
+    if (err) {
+        return ({"error":"Opening hw pin " + hwPin + " failed"});
+    }
+    gpio.write(hwPin, 1, function( err) {           // Set pin high (1)
+        if (err) {
+            return ({"error":"Setting pin " + hwPin + " failed"});
+        }
+        setTimeout(function (){                     // wait for toggleTime milliseconds and then execute
+            gpio.write(hwPin, 0, function() {       // Set pin low (0)
+                if (err) {
+                    return ({"error":"Setting pin " + hwPin + " failed"});
+                }
+                gpio.close(hwPin);                  // Close pin 
+            });
+        },toggleTime);
+        });
+   });
+}
+
 
 
 var express = require('express');
